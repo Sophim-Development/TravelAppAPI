0e9e99467e9dfbb3f2d913bba9183f845fbbe18d
Sophim Phath <165778058+SophimPhath@users.noreply.github.com>
2025-05-08 14:03:17 +0700

    fix: fix trips api test (#6)

--- src/validations/tripValidation.js
+++ src/validations/tripValidation.js
@@ -3,7 +3,7 @@ import { body, param } from 'express-validator';
 export const validateTripCreation = [
   body('title').notEmpty().withMessage('Title is required'),
   body('description').notEmpty().withMessage('Description is required'),
-  body('locationId').isInt().withMessage('Location ID must be an integer'),
+  body('locationId').isString().isUUID().withMessage('Location ID must be a valid UUID'),
   body('type').isIn(['bus', 'hotel']).withMessage('Type must be either "bus" or "hotel"'),
   body('startDate').isISO8601().withMessage('Start date must be a valid ISO8601 date'),
   body('endDate').isISO8601().withMessage('End date must be a valid ISO8601 date'),
@@ -11,5 +11,5 @@ export const validateTripCreation = [
 ];
 
 export const validateTripId = [
-  param('id').isInt().withMessage('Trip ID must be an integer'),
+  param('id').isString().isUUID().withMessage('Trip ID must be a valid UUID'),
 ];
